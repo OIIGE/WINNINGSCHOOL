1d41620daec4a7bd69dd9372d47d9ba9fd648c34
Oluwadamilare Ige <ige.oluwadamilarre@gmail.com>
2024-10-23 23:12:35 +0100

    WINNINGSCHOOL

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WINNINGSCHOOL.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
